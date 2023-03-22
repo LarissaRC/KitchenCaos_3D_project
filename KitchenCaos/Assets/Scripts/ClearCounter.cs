d0f2563ece11e87ed37755b66595b9206e7a46c1
LarissaRC <50839355+LarissaRC@users.noreply.github.com>
2023-03-22 08:50:56 -0400

    Placing objects in counters

--- KitchenCaos/Assets/Scripts/ClearCounter.cs
+++ KitchenCaos/Assets/Scripts/ClearCounter.cs
@@ -4,7 +4,46 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
+
+    private KitchenObject kitchenObject;
+
+    private void Update() {
+        if(testing && Input.GetKeyDown(KeyCode.T)) {
+            if(kitchenObject != null) {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
+
     public void Interact() {
-        Debug.Log("Ineract!");
+        if(kitchenObject == null){
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject) {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject() {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject() {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject() {
+        return kitchenObject != null;
     }
 }
