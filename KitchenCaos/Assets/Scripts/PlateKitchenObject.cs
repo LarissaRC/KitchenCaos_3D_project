541a910fddf8783943e1b68ef358435daefb9a4b
LarissaRC <50839355+LarissaRC@users.noreply.github.com>
2023-03-24 11:24:26 -0400

    Plate complete ingredients visual

--- KitchenCaos/Assets/Scripts/PlateKitchenObject.cs
+++ KitchenCaos/Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs {
+        public KitchenObjectSO kitchenObjectSO;
+    }
 
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
 
@@ -23,6 +29,10 @@ public class PlateKitchenObject : KitchenObject
             return false;
         } else {
             kitchenObjectSOList.Add(kitchenObjectSO);
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs {
+                kitchenObjectSO = kitchenObjectSO
+            });
+            
             return true;
         }
     }
